6b5f34bb3f87d389b22bc910846fac2135afc87e
Andy mgindotech <andy.mgindotech@gmail.com>
2022-11-03 17:19:23 +0700

    first commit

--- application/views/buku/daftarbuku.php
+++ application/views/buku/daftarbuku.php
@@ -1,34 +1,55 @@
 <?= $this->session->flashdata('pesan'); ?>
 
-<div style="padding: 25px;">
-    <div class="x_panel">
-        <div class="x_content">
-        <!-- Tampilkan semua produk -->
-            <div class="row">
-            <!-- looping products -->
-                <?php foreach ($buku as $buku) { ?>
-                <div class="col-md-2 col-md-3">
-                    <div class="thumbnail" style="height: 370px;">
-                        <img src="<?php echo base_url('assets/img/upload/') . $buku->image; ?>" style="max-width:100%; max-height: 100%; height: 200px; width: 180px">
-                        <div class="caption">
-                            <h5 style="min-height:30px;"><?= $buku->pengarang ?></h5>
-                            <h5><?= $buku->penerbit ?></h5>
-                            <h5><?= substr($buku->tahun_terbit, 0, 4) ?></h5><p>
-                            <?php
-                            if ($buku->stok < 1) 
-                            {
-                                echo "<i class='btn btn-outline-primary fas fw fa-shopping-cart'> Booking&nbsp;&nbsp 0</i>";
-                            } else {
-                                echo "<a class='btn btn-outline-primary fas fw fa-shopping-cart' href='" . base_url('booking/tambahBooking/' . $buku->id) . "'> Booking</a>";
-                            }
-                            ?>
-                            <a class="btn btn-outline-warning fas fw fa-search" href="<?= base_url('home/detailBuku/' . $buku->id); ?>"> Detail</a></p>
-                        </div>
-                    </div>
-                </div> 
-                <?php } ?>
-            <!-- end looping -->
+<div>
+    <div class="container d-flex" style="margin-top:-50px;width:100%; height:500px; background-image: url(<?= base_url('assets/img/bg-head.png') ?>);background-size:cover">
+        <div class="jb-head-1 d-flex align-items-center" style="height: 100% ; width: 100%">
+            <div class="text-set">
+                <h3>
+                    Social Media Academy
+                </h3>
+                <h5>
+                    Berkarir Sebagai Konten Kreator <br>
+                    Di Semua Platform.
+                </h5>
+                <p style="color: rgb(152, 152, 152);">Teknologilah yang bisa memberikan dampak sosial terbesar
+                    di negara ini, bukan kebijakan atau policy. - Nadiem Makarim</p>
+                <button style="background-color: #E7583C; outline:none; border:none; padding: 6px; border-radius:5px; color:#fff">Daftar Sekarang</button>
+                <button style="background-color: #fff; outline:none; border:2px solid #E7583C; padding: 6px; border-radius:5px; color:#E7583C">Selengkapnya</button>
             </div>
         </div>
+        <div class="jb-head-2 d-flex justify-content-between align-items-center" style="height: 100% ; width: 100%">
+            <div></div>
+            <img src="<?= base_url('assets/img/img-jb.png') ?>" alt="img" width="400" height="400">
+        </div>
+    </div>
+    <div class="soc-med d-flex flex-column align-items-center mt-5">
+        <h3>Platform Social Media</h3>
+        <img src="<?= base_url('assets/img/sm-head.png') ?>" alt="img" width="800">
+    </div>
+    <div id="carouselExampleIndicators" style="margin-top: 100px;" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="<?= base_url('assets/img/banner-1.png') ?>" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="<?= base_url('assets/img/banner-1.png') ?>" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="<?= base_url('assets/img/banner-1.png') ?>" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 </div>
--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -5,34 +5,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pustaka-Booking | <?= $judul; ?></title>
+    <title>Social Media Academy</title>
     <link rel="icon" type="image/png" href="<?= base_url('assets/img/logo/logo-pb.png'); ?>">
     <link rel="stylesheet" href="<?= base_url('assets/user/css/bootstrap.css'); ?>">
     <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css">
     <link href="<?= base_url('assets/datatable/datatables.css'); ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/css/global.css'); ?>" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="<?= base_url(); ?>">Pustaka</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="<?= base_url(); ?>">Beranda <span class="sr-only">(current)</span></a>
-                    <?php
-                    if (!empty($this->session->userdata('email'))) { ?>
-                        <a class="nav-item nav-link" href="<?= base_url('booking') ?>">Booking <b><?= $this->ModelBooking->getDataWhere('temp', ['email_user' => $this->session->userdata('email')])->num_rows(); ?></b> Buku</a>
-                        <a class="nav-item nav-link" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
-                        <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw fa-login"></i> Log out</a>
-                    <?php } else { ?>
-                        <a class="nav-item nav-link" data-toggle="modal" data-target="#daftarModal" href="#"><i class="fas fw fa-login"></i> Daftar</a>
-                        <a class="nav-item nav-link" data-toggle="modal" data-target="#loginModal" href="#"><i class="fas fw fa-login"></i> Log in</a>
-                    <?php } ?>
-                    <span class="nav-item nav-link nav-right" style="display:block; margin-left:20px;">Selamat Datang <b><?= $user; ?></b></span>
-                </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+        <div class="container d-flex justify-content-between">
+            <a class="navbar-brand" href="<?= base_url(); ?>">
+                <img src="<?= base_url('assets/img/logo-head.png') ?>" alt="logo" width="35">
+            </a>
+            <div class="link d-flex">
+                <a class="nav-item nav-link link-head" href="<?= base_url(); ?>">Beranda</a>
+                <a class="nav-item nav-link link-head" href="<?= base_url(); ?>">Program</span></a>
+                <a class="nav-item nav-link link-head" href="<?= base_url(); ?>">Events</span></a>
+                <a class="nav-item nav-link link-head" href="<?= base_url(); ?>">Tentang Kami</span></a>
+            </div>
+            <div class="navbar-nav">
+                <?php
+                if (!empty($this->session->userdata('email'))) { ?>
+                    <a class="nav-item nav-link" href="<?= base_url('booking') ?>">Booking <b><?= $this->ModelBooking->getDataWhere('temp', ['email_user' => $this->session->userdata('email')])->num_rows(); ?></b> Buku</a>
+                    <a class="nav-item nav-link" href="<?= base_url('member/myprofil'); ?>">Profil Saya</a>
+                    <a class="nav-item nav-link" href="<?= base_url('member/logout'); ?>"><i class="fas fw fa-login"></i> Log out</a>
+                <?php } else { ?>
+                    <button type="button" class="btn-head-login mr-3" style="height:50px">
+                        <a data-toggle="modal" data-target="#loginModal" href="#">Masuk</a>
+                    </button>
+                    <button type="button" class="btn-head-regist" style="height:50px">
+                        <a data-toggle="modal" data-target="#daftarModal" href="#">Daftar</a>
+                    </button>
+                <?php } ?>
             </div>
         </div>
     </nav>
